c878ce0bd0faa1a28936ffc1344d7575383dc8bc
syafiafp <syafiafp@gmail.com>
2022-04-25 23:38:57 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/pbm_b1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pbm_b1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:pbm_b1/welcome_screenpage/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Color(0xFF4CDBC4),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
+    );
+  }
+}
